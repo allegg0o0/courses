151b3fa66407bf53f6ca39a205c5eb54fb65715f
allegg0o0 <ol.turaew@yandex.ru>
2023-05-25 19:21:11 +0700

    created Index and Main
    basic data types

---
+++ main.js
@@ -0,0 +1,29 @@
+//1
+let month ="12";
+
+//2
+let greeting =  'Добро '+'пожаловать'+' на курс' + ' Олег' ;
+console.log(greeting);
+
+//3
+//alert("22.05.2023");
+
+//4
+document.getElementById('one').innerHTML = 'hello world!';
+
+
+//5
+let num = 111;
+let bNum = 999999999999999999999999n;
+let boolean = true;
+let str = "string_string";
+let undef;
+let nul = null;
+let sumb = '-';
+console.log(num);
+console.log(bNum);
+console.log(boolean);
+console.log(str);
+console.log(undef);
+console.log(nul);
+console.log(sumb);
