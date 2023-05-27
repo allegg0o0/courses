dfbc9f9dbfd2737a8b05b970a73ca419e336f06c
allegg0o0 <ol.turaew@yandex.ru>
2023-05-27 19:51:00 +0700

    lesson 4
    added addition of two numbers and comparison with third.
    comparison of two elements

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-document.querySelector('p').innerHTML = 'first';
-document.querySelectorAll('p')[1].innerHTML = 'second';
+
 let out = document.querySelector('.out');
-let inp = document.querySelector('.inp');
+let inp1 = document.querySelector('.inp1');
+let inp2 = document.querySelector('.inp2');
 let but = document.querySelector('.but');
 
-but.onclick= function() {
-if(inp.value>100)
-{out = inp.value;}
-else if(inp.value<=100)
-{console.log(inp.value);}
-else {document.querySelector('.out').innerHTML = 'не число';}
-inp.value='';
+let out2 = document.querySelector('.out2');
+let inp3 = document.querySelector('.inp3');
+let inp4 = document.querySelector('.inp4');
+let but2 = document.querySelector('.but2');
+
+but.onclick= function() 
+{
+if(inp1.value == "")
+{out.innerHTML = "нет числа в поле 1";}
+else if(inp2.value =="")
+{out.innerHTML = "нет числа в поле 2";}
+else if(inp1.value*1+inp2.value*1<15) 
+{out.innerHTML = inp1.value*1+inp2.value*1 + " меньше 15";}
+else if(inp1.value*1+inp2.value*1>=15)
+{out.innerHTML = inp1.value*1+inp2.value*1 + " больше 15";}
+}
+
+but2.onclick= function() 
+{
+    if(inp3.value===inp4.value)
+    {{out2.innerHTML = "элементы одинаковые";}}
+    else 
+    {out2.innerHTML = "элементы разные";}
 }
