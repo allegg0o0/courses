5ffe3873754a04ef9899dda0cc660ce445912f69
allegg0o0 <ol.turaew@yandex.ru>
2023-06-10 01:09:41 +0700

    lesson 9
    added arrays

--- main.js
+++ main.js
@@ -1,27 +1,73 @@
 let but1 = document.querySelector('.but1');
-let element = document.querySelector('.element');
-element.classList.add("size");
+let out =  document.querySelector('.out');
+let arr = [];
+let len = 15; //длина массива
+for(let i=0;i<len;i++)
+{
+    arr[i]=i+100;
+}
 but1.onclick = function()
 {
-    element.classList.toggle("size");
+    out = arr[5];
+    document.querySelector('.out').innerHTML = out;
 }
 
-////////////
+//////////
 
-let but2 = document.querySelector('.but2');
-let el2 = document.querySelector('.el2');
-el2.classList.add("size","color","shape");
+but2 = document.querySelector('.but2');
 but2.onclick = function()
 {
-    el2.classList.toggle("size");
-    el2.classList.toggle("shape");
-    el2.classList.toggle("color");
+    out = '';
+    for(let i=0;i<len;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            out += arr[i] + ' ';
+        }
+    }
+    document.querySelector('.out').innerHTML = out;
 }
 
-/////////////
+//////////
 
-del = document.querySelector('.del');
-del.onclick = function()
+but3 = document.querySelector('.but3');
+but3.onclick = function()
 {
-    el2.classList.remove("shape");
+    out = '';
+    for(let i=0;i<len;i++)
+    {
+        if(arr[i]%2==1)
+        {
+            out += arr[i] + ' ';
+        }
+    }
+    document.querySelector('.out').innerHTML = out;
+}
+
+//////////
+
+but4 = document.querySelector('.but4');
+let x =10; //высота массива 
+let y =4; //ширина массива
+let arr2 = new Array();
+arr2.length= x;
+for(let i=0;i<x;i++)
+{
+    arr2[i] = new Array();
+    arr2[i].length=y;
+    for(let j=0;j<y;j++)
+    {
+        arr2[i][j]=i+ '.'+ j + '    ';
+    }
+}
+but4.onclick = function()
+{
+    out = '';
+        for(let j=y-1;j>=0;j--)
+        {
+            
+            out+= arr2[2][j] + '___';
+            
+        }
+    document.querySelector('.out').innerHTML = out;
 }
