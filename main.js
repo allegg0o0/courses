4cb07fb7693e0625e8fa866fefa706ea1cac7a98
allegg0o0 <ol.turaew@yandex.ru>
2023-06-17 02:49:01 +0700

    lesson 12
    added event

--- main.js
+++ main.js
@@ -1,69 +1,44 @@
-let arr = [ "e", 'r', 'i', 'k', 't', 'h', 'e', 'b', 'e', 's', 't' ]
-let a = new Set();
-for(let i =0;i<arr.length;i++)
-{
-    a.add (arr[i]);
-}
-for (let val of a) document.write(val+ ' ');
-
-
-///////////////
-
+let circle = document.querySelector('.circle');
 let out;
-let inp=document.querySelector('.inp');
-let but=document.querySelector('.but');
-
-but.onclick = function()
+circle.onclick = function()
 {
-    if(inp.value !='')
+    circle.classList.add("resize");
+}
+
+///////////
+
+
+let ring = document.querySelector('.ring');
+let data = 123;
+ring.ondblclick = function()
+{
+    document.body.createTextNode = ('data');
+    console.log(data);
+    ring.classList.add("resize");
+    out = document.createTextNode('data = ' + data);
+    document.body.appendChild(out);
+}
+
+
+////////////
+
+let ring2 = document.querySelector('.ring2');
+ring2.classList.add("ring");
+ring2.onclick = function()
+{
+    ring2.classList.toggle("cat2");
+}
+
+//////////////
+
+inp = document.querySelector('.inp');
+inp.onkeypress = function(event)
+{
+    out = document.createTextNode(event.key+' ');
+    document.body.appendChild(out);
+    if(+event.key)
     {
-        out = document.createTextNode(inp.value+ ' \n');
+        out = document.createTextNode('false ' );
         document.body.appendChild(out);
     }
-}
-
-
-//////////////////
-
-
-let inp2=document.querySelector('.inp2');
-let but2=document.querySelector('.but2');
-
-but2.onclick = function()
-{
-    if(a.has(inp2.value))
-    {
-        out = document.createTextNode("true"+ ' \n');
-        document.body.appendChild(out);
-    }
-    else 
-    {
-        out = document.createTextNode("false"+ ' \n');
-        document.body.appendChild(out);
-    }
-}
-
-
-//////////////////////////
-
-
-let but3=document.querySelector('.but3');
- 
-let a2 = new Set();
-for(let i =0;i<20;i++)
-{
-    a2.add(i);
-}
-
-but3.onclick = function()
-{
-    console.log(a2)
-    for(let i of a2)
-{
-    if(i < 5)
-    {
-        a2.delete(i);
-    }
-}
-console.log(a2)
 }
