a9803690c846e62ada421fdce0becd97ee62bc64
allegg0o0 <ol.turaew@yandex.ru>
2023-07-14 16:44:17 +0700

    to do list
    created "to do list"

--- main.js
+++ main.js
@@ -1,28 +1,65 @@
-fetch('http://127.0.0.1:5500/')
-.then(data =>{
-console.log(data);
-console.log(data.text());
-})
+let but = document.querySelector('.plus');
+let inp = document.querySelector('.inp');
+let error = document.querySelector('.error');
+let arr =[];
+let point;
 
-// fetch('http://127.0.0.1:5500/')
-// .then(data =>{
-// console.log(data);
-// data.text().then(data2 =>{
-// console.log(data);
-// })
-// })
 
-// fetch('http://127.0.0.1:5500/')
-// .then(data =>{
-// console.log(data);
-// return data.text()
-// })
-// .then(data =>{
-// console.log(data);
-// })
+    function update()
+    {
+        document.querySelector('.listingContainer').innerHTML='';
+        for(let count=0; count<arr.length;count++)
+        {
+            
+            div = document.createElement('div');
+            div.classList.add('div'+count);
+            div.classList.add('div');
 
-fetch('http://127.0.0.1:5500/')
-.then(data => data.text())
-.then(data =>{
-console.log(data);
-})
+            li = document.createElement('li');
+            li.innerHTML=arr[count];
+            
+            gotovo = document.createElement('button');
+            gotovo.innerHTML = ' ✔';
+            gotovo.classList.add('gotovo');
+
+            gotovo.onclick = function()
+            {
+                document.querySelector(`.div${count}`).querySelector('li').classList.toggle('through');
+            }
+
+            del = document.createElement('button');
+            del.innerHTML = 'delete';
+            del.classList.add('del'+count);
+            del.classList.add('del');
+
+            del.onclick = function()
+            {
+                console.log(this.parentNode)
+                console.log()
+                this.parentNode.remove();
+                arr.splice(arr.indexOf(this.li),1);
+            }
+            
+            document.body.querySelector('.listingContainer').append(div);
+            document.body.querySelector(`.div${count}`).append(li);
+            document.body.querySelector(`.div${count}`).append(gotovo);
+            document.querySelector(`.div${count}`).append(del);
+        }
+    }
+    function inpCheck()
+    {
+        if(inp.value=='')
+        {error.innerHTML='Поле ввода не может быть пустым';}
+        else
+        {
+            error.innerHTML='';
+            arr.push(inp.value);
+        }
+    }
+    update();
+ but.onclick = function()
+{
+    inpCheck();  
+    update();
+    inp.value = ('');
+}
