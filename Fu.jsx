565d7b82df8b61fed215082b1496fbca8448cb87
allegg0o0 <ol.turaew@yandex.ru>
2023-07-27 23:51:07 +0700

    react 5

--- Fu.jsx
+++ Fu.jsx
@@ -1,7 +1,25 @@
+import React from "react";
+import Home from "./component/home";
+import Contact from "./component/contact";
+import About from "./component/about";
+import {HashRouter,  Route, Routes, NavLink} from "react-router-dom"
 const Fu = (props) => {
     return (
-        <div>
+        <div>          
             <h1>{props.name}</h1>
+            <HashRouter>
+            <ul>
+                <li><NavLink to={"/home"}>Home</NavLink></li>
+                <li><NavLink to={"/about"}>About</NavLink></li>
+                <li><NavLink to={"/contact"}>Contact</NavLink></li>
+            </ul>
+            
+            <Routes>
+            <Route path='/about' Component={About}/>
+            <Route path='/home' Component={Home}/>
+            <Route path='/contact' Component={Contact}/>
+            </Routes>
+            </HashRouter>
         </div>
     );
 }
